b8a9397eccad7104dad814a486897c27c97b5f31
quang_cao <tienquang@tuoitre.com.vn>
2021-05-20 11:25:36 +0700

    added code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+// const webpack = require('webpack');
 // const nodeExternals = require('webpack-node-externals'); // modules that should not be bundled.
 
 const VENDOR_LIBS = [
@@ -18,41 +19,59 @@ const VENDOR_LIBS = [
 module.exports = {
     mode: 'development',
     entry: {
-        bundle: "./src/index.js",
-        vendor: VENDOR_LIBS
+        bundle: "./src/index.js", // => entry chunk
+        // vendor: VENDOR_LIBS
     },
     output: {
         filename: "[name].[fullhash].js",
         path: path.resolve(__dirname, "dist"),
     },
+    devtool: 'eval-source-map',
     plugins: [
         new HtmlWebpackPlugin({
           template: "./src/index.html",
         }),
+        // use SourceMapDevToolPlugin for more configuration, set devtool: false
+        // new webpack.SourceMapDevToolPlugin({}),
     ],
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                loader: "babel-loader",
                 exclude: /node_modules/,
+                use: ['babel-loader', 'source-map-loader'],
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|mp3|ico)$/i,
                 use: {
                     loader: 'file-loader'
-                }
+                },
             }
         ]
     },
     optimization: {
+        // create new chunk contains un-change code (libraries) and share to another chunks
         splitChunks: {
-            // include all types of chunks
-            chunks: 'all',
+            // include all types of chunks (async, non-async chunk)
+            // chunks: 'all', 
+
+            // use cacheGroups instead, for naming and define library|folder,...
+            cacheGroups: {
+                // Create a vendors chunk, which includes all code from node_modules in the whole application,
+                // shared between entry points
+                commons: { // => cache group
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    // If the current chunk contains modules already split out from the main bundle,
+                    // it will be reused instead of a new one being generated
+                    reuseExistingChunk: true,
+                }
+            }
         },
     },
     devServer: {
@@ -61,3 +80,8 @@ module.exports = {
     // externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
     // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
 }
+
+/*
+    The SplitChunksPlugin allows us to extract common dependencies into an existing entry chunk or an entirely new chunk
+    New chunk can be shared OR modules are from the node_modules folder
+*/
